e8b4e385ead8399e48c2292ac1dc8efe0c9d4a72
shOuru4997 <souravkumar4997@gmail.com>
2021-11-15 21:46:40 +0530

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	'editor.formatOnSave': true,
+	proseWrap: 'always',
+};
